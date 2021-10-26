4912809c1a8880f23f47880d74f207db8bc84535
gabriela-aires <80225123+gabriela-aires@users.noreply.github.com>
2021-10-26 09:39:57 -0300

    26/10/2021

---
+++ aula_01/entradaviateclado/calculadora.js
@@ -0,0 +1,18 @@
+var num1 = parseInt(prompt('Digite o num1'));
+var num2 = parseInt(prompt('Digite o num2'));
+
+var soma = num1+num2;
+var subtração = num1-num2;
+var divisão = num1/num2;
+var multiplicação = num1*num2;
+
+
+document.getElementById('num1').innerHTML = num1;
+document.getElementById('num2').innerHTML = num2;
+document.getElementById('soma').innerHTML = soma;
+document.getElementById('subtração').innerHTML = subtracao;
+document.getElementById('divisão').innerHTML = divisao;
+document.getElementById('multiplicação').innerHTML = multiplicacao;
+
+
+//console.log(soma);
---
+++ aula_01/variaveis/exemplo01.js
@@ -0,0 +1,8 @@
+var texto = "Vamos fazer uma calculadora"
+var num1 = 25;
+var num2 = 26;
+var soma = num1+num2;
+alert(soma);
+//document.write(texto)
+console.log(num1);
+document.getElementById('teste').innerHTML = "Oi 1C, bom dia!"
